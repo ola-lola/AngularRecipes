0a2e4eafb4128b7318bf111dab762a12ce5b5f6d
ola-lola <olagrzegorczyk93@gmail.com>
2020-11-25 19:07:41 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { FormsModule } from '@angular/forms';
+import { DropDownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    DropDownDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { EventEmitter } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is a simple test', 'https://wszystkiegoslodkiego.pl/storage/images/201930/ciasto-bajka-1.jpg',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('Salt', 3)
+            ]),
+        new Recipe('A Test Recipe 2', 'This is a simple test', 'https://wszystkiegoslodkiego.pl/storage/images/201930/ciasto-bajka-1.jpg',
+            [
+                new Ingredient('Apple',5),
+                new Ingredient('Banana', 3)
+            ]),
+        
+      ];
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
+}
